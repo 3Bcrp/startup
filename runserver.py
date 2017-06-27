11f3f31a4837913e0348bbb5a174235eb0039ff9
Roches1990 <yo4uvak1990@gmail.com>
2017-06-27 16:00:00 +0500

    mend

--- runserver.py
+++ runserver.py
@@ -1,11 +1,4 @@
 from skynet import app
 import os
 
-base_path = os.path.dirname(os.path.abspath(__file__))
-# Folder creation
-try:
-    log_path = os.mkdir(os.path.join(base_path,'./skynet', './logs'))
-except OSError:
-    print('Path already exist')
-
 app.run(debug=True)
